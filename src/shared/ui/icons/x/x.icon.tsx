a9db03f240f1c8680be6f8bd09662ef8d33e31f4
Dx-connect <dx.connect@atomicmail.io>
2026-03-12 12:19:13 +0000

    chore:

---
+++ src/shared/ui/icons/x/x.icon.tsx
@@ -0,0 +1,7 @@
+import { WithScaleAndColor } from "../_shared/with-scale-and-color";
+
+const Xicon = WithScaleAndColor(
+	"M843-128L562-538l1 0 253-294h-85L524-606 360-832H138l263 383v0L124-128h85l230-267 183 267h221z M327-768l395 576h-67L260-768h67z",
+);
+
+export default Xicon;
--- src/shared/ui/sections/navbar/subcomponents/social-media.segment.tsx
+++ src/shared/ui/sections/navbar/subcomponents/social-media.segment.tsx
@@ -1,7 +1,40 @@
-const SocialMediaSegment = () => {
-    return (
-        <div>Social media</div>
-    )
-}
+import FacebookIcon from "../../../icons/facebook/facebook.icon";
+import MailCIon from "../../../icons/mail/mail.icon";
+import Xicon from "../../../icons/x/x.icon";
+import type { ReactNode } from "react";
 
-export default SocialMediaSegment
+type LinkType = {
+	url: string;
+	icon: ReactNode;
+};
+
+const LINKS: LinkType[] = [
+	{ url: "", icon: <Xicon scale={0.75} /> },
+	{ url: "", icon: <FacebookIcon scale={0.75}/> },
+	{ url: "", icon: <MailCIon scale={0.75} /> },
+];
+
+const SocialMediaSegment = () => {
+	return (
+		<div className="pe-4 flex space-x-2 text-emerald-700">
+			{LINKS.map((link: LinkType, index: number) => {
+				const Icon = link.icon;
+
+				return (
+					<a
+						className="flex justify-center items-center rounded p-0.5 hover:bg-emerald-300 duration-150 transition-colours"
+						key={`icon_${
+							// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+							index
+						}`}
+						href={link.url}
+					>
+						{Icon}
+					</a>
+				);
+			})}
+		</div>
+	);
+};
+
+export default SocialMediaSegment;
